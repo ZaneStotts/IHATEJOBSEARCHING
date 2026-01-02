509dec6f14c191109a94ce9fecf8da1b45e98291
ZaneStotts <ZaneStotts@users.noreply.github.com>
2026-01-02 02:55:41 -0600

    Update applications

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-/* Place your JavaScript in this file */
+// This variable constant represents applications list items
+const applications = document.querySelectorAll("ol > li");
+
+// This variable constant represents the "see more" button
+const button = getElementsByName("button");
