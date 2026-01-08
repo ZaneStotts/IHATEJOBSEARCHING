f9c8ea21edd6f3eae34e02abc604038e2aca2951
ZaneStotts <ZaneStotts@users.noreply.github.com>
2026-01-08 03:55:24 -0600

    Update applications

--- scripts.js
+++ scripts.js
@@ -2,4 +2,19 @@
 const applications = document.querySelectorAll("ol > li");
 
 // This variable constant represents the "see more" button
-const button = getElementsByName("button");
+const button = document.querySelector(".button-container button");
+
+let visibleApplications = 11;
+
+showApplications();
+
+button.addEventListener("click", () => {
+    visibleApplications += 10;
+    showApplications();
+})
+
+function showApplications() {
+    applications.forEach((application, index) => {
+        application.style.display = index < visibleApplications ? "block" : "none";
+    });
+}
