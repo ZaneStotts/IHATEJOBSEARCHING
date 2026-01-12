154cfa55942a19f2f53080ac3810e5a158cc6ea8
ZaneStotts <ZaneStotts@users.noreply.github.com>
2026-01-12 04:25:58 -0600

    Update comments

--- scripts.js
+++ scripts.js
@@ -8,13 +8,17 @@ const button = document.querySelector(".button-container button");
 // I chose number 11 because index starts at 0
 let visibleApplications = 11;
 
+// This calls a function in order to render the first set of visible applications
 showApplications();
 
+// This creates event listener so 10 more applications are made visible when "see more" button is clicked
 button.addEventListener("click", () => {
     visibleApplications += 10;
     showApplications();
 })
 
+// This function loops through every <li> item and changes its display style from "none" to "block"
+// "index" is a local variable representing the numeric position of the current <li> item
 function showApplications() {
     applications.forEach((application, index) => {
         application.style.display = index < visibleApplications ? "block" : "none";
