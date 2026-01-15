f07da33ee0f212fa4ee74a33abe75b3ccf6600e7
ZaneStotts <ZaneStotts@users.noreply.github.com>
2026-01-15 03:56:01 -0600

    Update comments and scripts

--- scripts.js
+++ scripts.js
@@ -1,35 +1,49 @@
-// This variable constant represents the <p> tag with the application number inside
-const appNumber = document.querySelector(".job-number-container p");
+function countAnimation(htmlElement, targetNumber, animationDuration) {
 
-// This variable represents the number currently being shown
-// It is a let variable because the value changes
-let currentAppNumber = 0;
+    // This variable represents the number currently being shown
+    // It is a let variable because the value changes
+    let currentNumber = 0;
 
-// This variable constant represents the number I want the counting animation to reach
-const appNumberTarget = 736;
+    // This variable constant represents an estimate of how often the browser updates the screen
+    // It helps calculate how much the number in the counting animation should increase per frame
+    const frameRate = 1000 / 60;
 
-// This variable constant represents in milliseconds how long it takes to complete the counting animation
-const appNumberAnimationDuration = 1500;
+    // This variable constant represents how much the number should increase each frame
+    const animationIncrement = targetNumber / (animationDuration / frameRate);
 
-// This variable constant represents an estimate of how often the browser updates the screen
-// It helps calculate how much the number in the counting animation should increase per frame
-const frameRate = 1000 / 60;
+    function countNumbers() {
+        currentNumber += animationIncrement;
 
-// This variable constant represents how much the number should increase each frame
-const appNumberIncrement = appNumberTarget / (appNumberAnimationDuration / frameRate);
-
-function appNumberAnimation() {
-    currentAppNumber += appNumberIncrement;
-
-    if (currentAppNumber >= appNumberTarget) {
-        appNumber.textContent = appNumberTarget;
-    } else {
-        appNumber.textContent = Math.floor(currentAppNumber);
-        requestAnimationFrame(appNumberAnimation);
+        if (currentNumber >= targetNumber) {
+            htmlElement.textContent = targetNumber;
+        } else {
+            htmlElement.textContent = Math.floor(currentNumber);
+            requestAnimationFrame(countNumbers);
+        }
     }
+
+    countNumbers();
+
 }
 
-appNumberAnimation();
+/*
+
+// This variable constant represents the <p> tag with application number inside
+const appNumber = document.querySelector(".job-number-container p");
+
+// This variable constant represents the <p> tag with interview number inside
+const interviewNumber = document.querySelector("interview-number-container p");
+
+// This variable constant represents the number I want the counting animation to reach for applications
+const appNumberTarget = 736;
+
+// This variable constant represents the number I want the counting animation to reach for interviews
+const interviewNumberTarget = 9;
+
+// This variable constant represents in milliseconds how long it takes to complete the counting animation
+const countAnimationDuration = 2000;
+
+*/
 
 // This variable constant represents applications list items
 const applications = document.querySelectorAll("ol > li");
