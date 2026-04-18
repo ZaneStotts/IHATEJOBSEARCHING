f259b460ee05bc2ca86c31c3b8461c1b9511c6ae
ZaneStotts <ZaneStotts@users.noreply.github.com>
2026-04-18 14:53:42 -0500

    Update applications and scripts

--- scripts.js
+++ scripts.js
@@ -115,69 +115,33 @@ darkModeButton.addEventListener("click", () => {
 
 
 
-// WELCOME PAGE NUMBER COUNTER FEATURE
+// APPLICATION COUNTER FEATURE
 
-function countAnimation(htmlElement, targetNumber, animationDuration) {
+// This function counts the number of applications and displays them
+function countTotalApplications() {
 
-    // This variable represents the number currently being shown
-    // It is a let variable because the value changes
-    let currentNumber = 0;
+    // This constant variable represents all elements with the ".application" class
+    const applications = document.querySelectorAll(".application");
 
-    // This variable constant represents an estimate of how often the browser updates the screen
-    // It helps calculate how much the number in the counting animation should increase per frame
-    const frameRate = 1000 / 60;
+    // This constant variable represents the applications total
+    const applicationsTotal = applications.length;
 
-    // This variable constant represents how much the number should increase each frame
-    const animationIncrement = targetNumber / (animationDuration / frameRate);
+    // This constant variable decides where the total number should be displayed in the HTML
+    const output = document.querySelector(".job-number-container p");
 
-    // This function updates the number once per animation frame
-    function countNumbers() {
-        currentNumber += animationIncrement;
-
-        // Tells function to stop the animation if the final number is reached
-        if (currentNumber >= targetNumber) {
-            htmlElement.textContent = targetNumber;
-        } else {
-
-            // Used "Math.floor" or else decimals would appear on screen
-            htmlElement.textContent = Math.floor(currentNumber);
-
-            // This line tells the browser to run the function again on the next animation frame
-            requestAnimationFrame(countNumbers);
-        }
+    // If the element (".job-number-container p") exists , then update the text content to display the total amount of applications
+    if (output) {
+        output.textContent = applicationsTotal;
     }
 
-    // This calls function for the first time so the loop can start
-    countNumbers();
-
+    // Returns the total amount of applications in case it needs to be used in the future
+    return applicationsTotal;
 }
 
-// This variable constant represents the <p> tag with application number inside
-const applicationNumber = document.querySelector(".job-number-container p");
-
-// This variable constant represents the <p> tag with interview number inside
-const interviewNumber = document.querySelector(".interview-number-container p");
-
-// This variable constant represents the <p> tag with the offer number inside
-const offerNumber = document.querySelector(".offer-number-container p");
-
-// This line controls animation delay time for applications
-setTimeout(() => {
-    applicationNumber.style.opacity = "1";
-    countAnimation(applicationNumber, 738, 3000);
-}, 500);
-
-// This line controls animation delay time for interviews
-setTimeout(() => {
-    interviewNumber.style.opacity = "1";
-    countAnimation(interviewNumber, 9, 300);
-}, 4000);
-
-// This line controls animation delay for offers
-setTimeout(() => {
-    offerNumber.style.opacity = "1";
-    countAnimation(offerNumber, 1, 50);
-}, 5000);
+// This group of lines makes sure the HTML is loaded before running the previously described function
+document.addEventListener("DOMContentLoaded", () => {
+    countTotalApplications();
+});
 
 
 
